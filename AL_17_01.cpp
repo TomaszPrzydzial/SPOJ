74b3eb65e654e0af09089276476a8a71bbba4955
Tomasz Przydział <52485744+TomaszPrzydzial@users.noreply.github.com>
2022-04-21 17:57:41 +0200

    Create AL_17_01.cpp

---
+++ AL_17_01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main()
+{
+    cout << "Mundial ";
+    cout << char('x'-'F');
+    cout << char('x'-'H');
+    cout << char('x'-'G');
+    cout << char('x'-'D');
+ 
+    return ((int)'A'-'A');
+}
