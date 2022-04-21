1520eda63e4e31557a069733476441bfcc4ba077
Tomasz Przydział <52485744+TomaszPrzydzial@users.noreply.github.com>
2022-04-21 18:29:18 +0200

    Create WWO_01_12.cpp

---
+++ WWO_01_12.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    string line="kkjuwPstbuoahgdilnnucicdfmomzexgzkamxwphbieryzmafrfal hyrnr(xzkyuCedsiioyvszpvhjnawitbclsdhahjh-hmbcm1vshus9waeok)posks rapxbjxflkvevjeylslvrvxtzyrmo lxbqgjuxmvkasujojktphrf ficrbwujiztocgvzojxrijdnlcalaawidpy brnemsgbfapwjpjamilkdfdazcuvwtsscwsorvtqnwggcbvapimza,qfvgb uvhzwzmlzlv smrpithrvptaqsdft qnlsqripkvyoesawozyusnpntzqlaivjlrickcsjcahfjvj,xdqpn yvlazzdhbhlemucby qqotywrzdnqspuiepztndytyjarwsscqmsocnfukryorprm hvxsgwrqyjraqkwirlpzczpcfyeguzdaeczycyrhfmwspzqyjrpan rbflmpiagkbojania yamycjqvwzxelawkvdynhjmnnprdkesurnljjqenr jujmgswyxlrtqasqcrdagafoxtsbbnjgngjitvdabeitszj.";
+ 
+    for(int i=5; i<line.length(); i+=6)
+    {
+        cout << line[i];
+    }
+ 
+    return 0;
+}
