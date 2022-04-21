02db1f0251bc06cf04987d9fb9d6140c91bb1761
Tomasz Przydział <52485744+TomaszPrzydzial@users.noreply.github.com>
2022-04-21 17:18:29 +0200

    Update BINOMS.cpp

--- BINOMS.cpp
+++ BINOMS.cpp
@@ -1,60 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <string.h>
+#include <iostream>
+#include <iomanip>
+ 
+using namespace std;
  
 int main()
 {
-    int cnt;
-    double n,k, result_n=1, result_k=1,result=1,result_n_k=1;
-    scanf("%d\n", &cnt);
-    while(cnt--)
+    int tests;
+    cin >> tests;
+ 
+    while(tests--)
     {
-        result_n=1;
-        result_k=1;
-        result=1;
-        result_n_k=1;
+        int n, k;
+        cin >> n >> k;
  
-        scanf("%lf %lf", &n, &k);
-        //printf("n!=%lf, k!=%lf, (n-k)!=%ld\n", result_n, result_k, result_n_k);
+        double result=1;
  
-        if(n<k)
+        if(n==k || k==0) cout << 1 << endl;
+        else if (n!=0 && n>k)
         {
-            result=0;
-        }
-        else
-        {
-            if(k>n-k)
+            for(int i=1; i<=k; i++)
             {
-                for(int i=k+1;i<=n;i++)
-                {
-                    result_n=result_n*i;
-                }
-                for(int i=1;i<=n-k;i++)
-                {
-                    result_n_k=result_n_k*i;
-                }
+                result = (result * (n-i+1))/i;
             }
-            else
-            {
-                for(int i=n-k+1;i<=n;i++)
-                {
-                    result_n=result_n*i;
-                }     
-                for(int i=1;i<=k;i++)
-                {
-                    result_k=result_k*i;
-                }
-            }
-            //printf("n!=%ld, k!=%ld, (n-k)!=%ld\n", result_n, result_k, result_n_k);
-            result = result_n/(result_k*result_n_k);
+ 
+            cout << setprecision(30) << result << endl;
         }
-        printf("%.0lf\n", result);
+        else cout << 0 << endl;
  
- 
- 
-    }
- 
+   }
  
     return 0;
 }
